2fe56e85331e7ba4d1692eb1ce111fe272656d5a
Aleksei1987-lab <121310615+Aleksei1987-lab@users.noreply.github.com>
2024-09-26 22:16:58 +0300

    Delete vite-hangman directory

--- vite-hangman/src/js/consts.js
+++
@@ -1,5 +0,0 @@
-
-export const WORDS = ['code', 'frontend', 'programming'];
-
-
-export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
--- vite-hangman/src/js/main.js
+++
@@ -1,9 +0,0 @@
-
-import '../css/style.css';
-import { darkModeHandle } from './utils';
-import { startGame } from './game';
-
-darkModeHandle();
-
-const startGameBtn = document.getElementById('startGame');
-startGameBtn.addEventListener('click', startGame);
